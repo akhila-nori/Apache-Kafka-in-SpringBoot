88ca824f9793efc0fa02c329e07ff351b696531c
akhila-nori <akhilanori2001@gmail.com>
2025-12-27 22:15:59 +0530

    User service - controller, Kafka topic confi - for kafka config and application.properties for mysql datasource URL config added

---
+++ user-service/src/main/java/com/kafkaImpl/user_service/controller/UserController.java
@@ -0,0 +1,33 @@
+package com.kafkaImpl.user_service.controller;
+
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+@RequestMapping("/users")
+@RequiredArgsConstructor
+public class UserController {
+
+    @Value("${kafka.topic.user-random_topic}")
+    private String KAFKA_RANDOM_USER_TOPIC;
+
+    //KafkaTemplate would be injected by SpringBoot framework on our behalf because we have written a @RequiredArgsConstructor
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    @PostMapping("/{message}")
+    public ResponseEntity<String> sendMessage(@PathVariable String message){
+
+        kafkaTemplate.send(KAFKA_RANDOM_USER_TOPIC,message);
+
+        return ResponseEntity.ok("Message queued");
+    }
+}
